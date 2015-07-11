ce17b16e7909648aaa098e16c866492717cf2672
jiaoling <cjlchaos@gmail.com>
2015-07-10 23:15:13 -0400

    deleted two migrations which incurred errors

--- project/course/urls.py
+++ project/course/urls.py
@@ -6,5 +6,5 @@ from course import views
 urlpatterns=patterns(
     'course.views',
     url(r'^$', views.course_list, name='courses'),
-    url(r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/$', views.course_view, name='course_view'),
+    url(r'^(?P<slug>\w+)-(?P<pk>\d+)/$', views.course_view, name='course_view'),
     )

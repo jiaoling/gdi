07ee38938550a01afa3fc85f2728a0862fe3bb79
jiaoling <cjlchaos@gmail.com>
2015-07-13 10:25:40 -0400

    Merged master fixed conflict

--- project/course/urls.py
+++ project/course/urls.py
@@ -5,20 +5,12 @@ from course import views
 urlpatterns=patterns(
     'course.views',
     url(r'^$', views.course_list, name='courses'),
-<<<<<<< HEAD
-    url(r'^(?P<slug>\w+)-(?P<pk>\d+)/$', views.course_view, name='course_view'),
-=======
     url(r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/$', views.course_view, name='course_view'),
 
     # url(r'^create/$', 'create_course',
     #     name="create_course"),
     url(r'^edit/(?P<slug>[-\w]+)-(?P<course_id>\d+)/$', 'edit_course',
         name='edit_course'),
-<<<<<<< HEAD
-    url(r'^delete/(?P<slug>[-\w]+)-(?P<course_id>\d+)/$', 'delete_course',
-        name='delete_course'),
->>>>>>> july_11th_branch
-=======
     # url(r'^delete/(?P<slug>[-\w]+)-(?P<course_id>\d+)/$', 'delete_course',
     #     name='delete_course'),
 
@@ -28,5 +20,4 @@ urlpatterns=patterns(
         name='edit_material'),
     url(r'^material_delete/(?P<slug>[-\w]+)-(?P<material_id>\d+)/$', 'delete_material',
         name='delete_material'),
->>>>>>> user_authentication_branch
     )

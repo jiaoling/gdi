90f57c53643164efafd8c06eece677b5bf014113
jiaoling <cjlchaos@gmail.com>
2015-07-13 10:30:20 -0400

    Merged master fixed conflict

--- project/instructor/urls.py
+++ project/instructor/urls.py
@@ -10,26 +10,12 @@ from instructor import views
 urlpatterns=patterns(
     'instructor.views',
     url(r'^$', views.instructor_list, name='instructors'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-    url(r'^(?P<slug>[-\w\d]+),(?P<pk>\d+)/$', views.instructor_view)
-=======
-    url(r'^(?P<slug1>[-\w]+)-(?P<slug2>[-\w]+)-(?P<pk>\d+)/$', views.instructor_view),
-=======
     url(r'^(?P<slug1>[-\w]+)-(?P<slug2>[-\w]+)-(?P<pk>\d+)/$','instructor_view', name='instructor_view'),
 
->>>>>>> user_authentication_branch
-    url(r'^create/$', 'create_instructor',
-        name="create_instructor"),
+    # url(r'^create/$', 'create_instructor',
+    #     name="create_instructor"),
     url(r'^edit/(?P<slug1>[-\w]+)-(?P<slug2>[-\w]+)-(?P<instructor_id>\d+)/$', 'edit_instructor',
         name='edit_instructor'),
-    url(r'^delete/(?P<slug1>[-\w]+)-(?P<slug2>[-\w]+)-(?P<instructor_id>\d+)/$', 'delete_instructor',
-        name='delete_instructor'),
-<<<<<<< HEAD
->>>>>>> july_11th_branch
+    # url(r'^delete/(?P<slug1>[-\w]+)-(?P<slug2>[-\w]+)-(?P<instructor_id>\d+)/$', 'delete_instructor',
+    #     name='delete_instructor'),
     )
-=======
-
-
-    )
->>>>>>> user_authentication_branch

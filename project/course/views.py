193266e846bd1ee0c8840642c4fcb8f53874d5d6
ecboettch <ecboettch@users.noreply.github.com>
2015-07-25 20:43:22 -0400

    Update views.py
    Changes: course_list uses render() instead of HttpResponse(), course_view() requires authentication of user

--- project/course/views.py
+++ project/course/views.py
@@ -8,23 +8,34 @@ from slugify import slugify
 
 def course_list(request):
     courses = Course.objects.all().order_by('start_date')
-    t = loader.get_template('course_list.html')
+    #t = loader.get_template('course_list.html')
     # added topics and instructors so the template could access names of those objects
-    c = Context({'courses': courses})
-    return HttpResponse(t.render(c))
+    #c = Context({'courses': courses})
+    #return HttpResponse(t.render(c))
+    return render(request, 'course_list.html', {'courses': courses})
 
 def course_view(request,slug, pk):
     course = get_object_or_404(Course, pk=pk)
     links = Material.objects.filter(type= 'link', course_id = pk)
     files = Material.objects.filter(type= 'file', course_id = pk)
-    return render(request, 'course_view.html', {'course':course,
-                                                'pk':pk,
-                                                'links':links,
-                                                'files':files})
+    if course.instructor.user == request.user:
+        return render(request, 'course_view.html', {'course':course,
+                                                    'pk':pk,
+                                                    'links':links,
+                                                    'files':files})
+    else:
+        return render(request, 'course_view_anon.html', {'course':course,
+                                                    'pk':pk,
+                                                    'links':links,
+                                                    'files':files})
 
 def redirect_to_page(request, pk):
     course = get_object_or_404(Course, pk=pk)
-    return render(request, 'course_view.html', {'course_page':course, 'pk':pk})
+    if course.instructor.user == request.user:
+        return render(request, 'course_view.html', {'course_page':course, 'pk':pk})
+    
+    else:
+         return render(request, 'course_view_anon.html', {'course_page':course, 'pk':pk})
 
 # course
 # def create_course(request):

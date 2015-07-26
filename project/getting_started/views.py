eba3d935357d7ca0bf0b927aed1980d8309fe102
ecboettch <ecboettch@users.noreply.github.com>
2015-07-25 21:12:06 -0400

    Update views.py
    updates getting_started() to use render() instead of HttpResponse()

--- project/getting_started/views.py
+++ project/getting_started/views.py
@@ -6,6 +6,7 @@ from course.models import *
 # Create your views here.
 def getting_started(request):
     courses = Course.objects.all().order_by('c_name')
-    t= loader.get_template('getting_started.html')
-    c= Context({'getting_started': courses,})
-    return HttpResponse(t.render(c))
+    #t= loader.get_template('getting_started.html')
+    #c= Context({'getting_started': courses,})
+    #return HttpResponse(t.render(c))
+    return render(request, 'getting_started.html', {'courses': courses})
